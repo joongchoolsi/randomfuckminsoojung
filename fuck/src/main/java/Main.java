4f9373e1aecf8e5b1b88f140e3ff18b4d24c85f1
joongchoolsi <118674213+joongchoolsi@users.noreply.github.com>
2022-11-26 00:29:11 +0900

    Add files via upload

---
+++ fuck/src/main/java/Main.java
@@ -0,0 +1,125 @@
+
+
+public class Main {
+    public static String Random_Fuck_Min_Soo_Jung(int count) {
+        StringBuilder sb = new StringBuilder();
+        count--;
+        for (int i = 0; i <= count; i++) {
+            String[] a = {"분노 조절 못하고", "고집만 #가앙조 쎄고", "틀니 딱딱 소리 #가앙조 시끄럽고", "친구 없고", "얼굴 #이상하게 생겼고", "#욕수정1", "자기 말만 맞다고 생각하고", "자기와 의견 다르면 무조건 틀렸다 생각하고", "타인 존중 #강조 안하고", "말 한 번이라도 안 듣거나 까먹으면 급발진하고", "맨날 최소 1번은 극대노하고", "#온라인남친1", "학생의 학 자만 들어도 극혐하고", "대부분의 말이 논리는 변기통에 버려놓고 말도 안되는데 화만 내고", "학생 진로 존중 절대 안하고 그냥 공부나 쳐 하라고 하고", "쉬는시간에 불 끄고 의자 좀 옮겼다고 지 혼자 급발진해서 종례 30분 늦게 하고", "반티 5천원 이하로 사라고 극대노하면서 정신병자냐며 욕하고", "한 명이 청소 안 하고 도망갔다고 반 애들 전부 청소시키고", "지 잘못 #강조 모르고", "쿨하지도 재밌지도 않은데 쿨하고 재밌는 척만 겁나게 해대고", "본인은 자기 말투 재밌다고 생각하는데 그냥 #이상하기만 하고", "#카톡친구1", "뇌 없고", "1대 1에서는 분노조절 못하지만 상대가 여럿일 때는 분노조절 잘하고", "간단한 질문만 해도 극대노하고", "지구과학 교과서 외우고 배끼는 것 빼고는 할 줄 아는 게 없고", "어차피 나중에 성적 나오는 모의고사 채점 안 했다고 청소 10번 시키고", "#뭐만하면 청소 추가라면서 청소로 협박해대고", "얼굴이 태어나다 만, 잘못 생긴 모습이고", "학생 사람취급 #강조 안 하고", "누구나 일반적이고 정상적으로, 아니면 재밌게 대답해 줄 수 있는 질문에도 일단 급발진부터 박고", "칠판에서 오목만 둬도 칠판에다가 뭔 개지랄하는 거냐면서 욕하고", "뇌 없고", "숨어있는 쓰레기 하나 못 찾았다고 정신병자냐면서 극대노하고", "지가 먼저 말한 약속도 안지키면서 남한테는 존나 빡빡하고", "걸레질 좀 살살했다고 병신새끼냐며 극대노하고", "지 번호 저장 안해놨다고 교무실에서 1시간동안 가만히 서있게 하고", "카톡할 때 말투 개 병신 같고"}; //카톡할때말투: 38                                                                                                                                               ㅁ
+            String[] b = {"분노 조절 못하는", "고집만 #가앙조 쎈", "틀니 딱딱 소리 #가앙조 시끄러운", "친구 없는", "얼굴 #이상하게 생긴", "#욕수정2", "자기 말만 맞다고 생각하는", "자기와 의견 다르면 무조건 틀렸다 생각하는", "타인 존중 #강조 안하는", "말 한 번이라도 안 듣거나 까먹으면 급발진하는", "맨날 최소 1번은 극대노하는", "#온라인남친2", "학생의 학 자만 들어도 극혐하는", "대부분의 말이 논리는 변기통에 버려놓고 말도 안되는데 화만 내는", "학생 진로 존중 절대 안하고 그냥 공부나 쳐 하라고 하는", "쉬는시간에 불 끄고 의자 좀 옮겼다고 지 혼자 급발진해서 종례 30분 늦게 하는", "반티 5천원 이하로 사라고 극대노하면서 정신병자냐며 욕하는", "한 명이 청소 안 하고 도망갔다고 반 애들 전부 청소시키는", "지 잘못 #강조 모르는", "쿨하지도 재밌지도 않은데 쿨하고 재밌는 척만 겁나게 해대는", "본인은 자기 말투 재밌다고 생각하는데 그냥 #이상하기만 한", "#카톡친구2", "뇌 없는", "1대 1에서는 분노조절 못하지만 상대가 여럿일 때는 분노조절 잘하는", "간단한 질문만 해도 극대노하는", "지구과학 교과서 외우고 배끼는 것 빼고는 할 줄 아는 게 없는", "어차피 나중에 성적 나오는 모의고사 채점 안 했다고 청소 10번 시키는", "#뭐만하면 청소 추가라면서 청소로 협박해대는", "얼굴이 태어나다 만, 잘못 생긴 모습인", "학생 사람취급 #강조 안 하는", "누구나 일반적이고 정상적으로, 아니면 재밌게 대답해 줄 수 있는 질문에도 일단 급발진부터 박는", "칠판에서 오목만 둬도 칠판에다가 뭔 개지랄하는 거냐면서 욕하는", "뇌 없는", "숨어있는 쓰레기 하나 못 찾았다고 정신병자냐면서 극대노하는", "지가 먼저 말한 약속도 안지키면서 남한테는 존나 빡빡한", "걸레질 좀 살살했다고 병신새끼냐며 극대노하는", "지 번호 저장 안해놨다고 교무실에서 1시간동안 가만히 서있게 하는", "카톡할 때 말투 개 병신 같은"};
+            String[] c = {"분조장", "애", "아픈 사람", "정신병자", "년", "새끼", "틀딱", "유사 선생", "년"};
+            int aint = (int) (Math.random() * 38);
+            int bint = (int) (Math.random() * 38);
+            while (bint == aint) bint = (int) (Math.random() * 38);
+            int cint = (int) (Math.random() * 8);
+            String t = a[aint] + " " + b[bint] + " " + c[cint];
+
+            if (t.contains("#온라인남친1")) {
+                int onlineBf1 = (int) (Math.random() * 4);
+                if (onlineBf1 == 1) t = t.replace("#온라인남친1", "메이플에서 남친 구하고 있고");
+                else if (onlineBf1 == 2) t = t.replace("#온라인남친1", "메이플에서 친구 구하고 있고");
+                else if (onlineBf1 == 3) t = t.replace("#온라인남친1", "커뮤니티 사이트에서 남친 구하고 있고");
+                else if (onlineBf1 == 4) t = t.replace("#온라인남친1", "유튜브 댓글창에서 남친 구하고 있고");
+                else if (onlineBf1 == 0) t = t.replace("#온라인남친1", "카트라이더에서 남친 구하고 있고");
+            }
+            if (t.contains("#온라인남친2")) {
+                int onlineBf2 = (int) (Math.random() * 4);
+                if (onlineBf2 == 1) t = t.replace("#온라인남친2", "메이플에서 남친 구하고 있는");
+                else if (onlineBf2 == 2) t = t.replace("#온라인남친2", "메이플에서 친구 구하고 있는");
+                else if (onlineBf2 == 3) t = t.replace("#온라인남친2", "커뮤니티 사이트에서 남친 구하고 있는");
+                else if (onlineBf2 == 4) t = t.replace("#온라인남친2", "유튜브 댓글창에서 남친 구하고 있는");
+                else if (onlineBf2 == 0) t = t.replace("#온라인남친2", "카트라이더에서 남친 구하고 있는");
+            }
+            if (t.contains("#카톡친구1")) {
+                int secAc = (int) (Math.random() * 5) + 1;
+                int family = (int) (Math.random() * 2) + 2;
+                int blocked = (int) (Math.random() * 9) + 1;
+                int onlineFr = (int) (Math.random() * 3) + 1;
+                int total = secAc + family + blocked + onlineFr;
+                t = t.replace("#카톡친구1", "카톡친구 '가족 " + family + "명, 온라인친구 " + onlineFr + "명, 친구였지만 차단당한 사람 " + blocked + "명, 부계정 " + secAc + "명', 이렇게 " + total + "명이고");
+            }
+            if (t.contains("#카톡친구2")) {
+                int secAc = (int) (Math.random() * 5) + 1;
+                int family = (int) (Math.random() * 2) + 2;
+                int blocked = (int) (Math.random() * 9) + 1;
+                int onlineFr = (int) (Math.random() * 3) + 1;
+                int total = secAc + family + blocked + onlineFr;
+                t = t.replace("#카톡친구2", "카톡친구 '가족 " + family + "명, 온라인친구 " + onlineFr + "명, 친구였지만 차단당한 사람 " + blocked + "명, 부계정 " + secAc + "명', 이렇게 " + total + "명인");
+            }
+            if (t.contains("#욕수정1")) {
+                int insult = (int) (Math.random() * 9) + 1;
+                if (insult == 1) t = t.replace("#욕수정1", "#뭐만하면 정신병자라고 욕하고");
+                else if (insult == 2) t = t.replace("#욕수정1", "#뭐만하면 정신병 있냐고 욕하고");
+                else if (insult == 3) t = t.replace("#욕수정1", "#뭐만하면 병신이라고 욕하고");
+                else if (insult == 4) t = t.replace("#욕수정1", "#뭐만하면 병신새끼라고 욕하고");
+                else if (insult == 5) t = t.replace("#욕수정1", "#뭐만하면 씨발새끼라고 욕하고");
+                else if (insult == 6) t = t.replace("#욕수정1", "#뭐만하면 정신병자라고 욕해대고");
+                else if (insult == 7) t = t.replace("#욕수정1", "#뭐만하면 정신병 있냐고 욕해대고");
+                else if (insult == 8) t = t.replace("#욕수정1", "#뭐만하면 병신새끼라고 욕해대고");
+                else if (insult == 9) t = t.replace("#욕수정1", "#뭐만하면 병신이라고 욕해대고");
+                else if (insult == 10) t = t.replace("#욕수정1", "#뭐만하면 씨발새끼라고 욕해대고");
+            }
+            if (t.contains("#욕수정2")) {
+                int insult = (int) (Math.random() * 9) + 1;
+                if (insult == 1) t = t.replace("#욕수정2", "#뭐만하면 정신병자라고 욕하는");
+                else if (insult == 2) t = t.replace("#욕수정2", "#뭐만하면 정신병 있냐고 욕하는");
+                else if (insult == 3) t = t.replace("#욕수정2", "#뭐만하면 병신이라고 욕하는");
+                else if (insult == 4) t = t.replace("#욕수정2", "#뭐만하면 병신새끼라고 욕하는");
+                else if (insult == 5) t = t.replace("#욕수정2", "#뭐만하면 씨발새끼라고 욕하는");
+                else if (insult == 6) t = t.replace("#욕수정2", "#뭐만하면 정신병자라고 욕해대는");
+                else if (insult == 7) t = t.replace("#욕수정2", "#뭐만하면 정신병 있냐고 욕해대는");
+                else if (insult == 8) t = t.replace("#욕수정2", "#뭐만하면 병신이라고 욕해대는");
+                else if (insult == 9) t = t.replace("#욕수정2", "#뭐만하면 병신새끼라고 욕해대는");
+                else if (insult == 10) t = t.replace("#욕수정2", "#뭐만하면 씨발새끼라고 욕해대는");
+            }
+            if (t.contains("#강조")) {
+                int insult = (int) (Math.random() * 5) + 1;
+                if (insult == 1) t = t.replace("#강조", "죽어도");
+                else if (insult == 2) t = t.replace("#강조", "절대로");
+                else if (insult == 3) t = t.replace("#강조", "죽을 때까지");
+                else if (insult == 4) t = t.replace("#강조", "아예");
+                else if (insult == 5) t = t.replace("#강조", "ㅈ도");
+                else t = t.replace("#강조", "");
+            }
+            if (t.contains("#가앙조")) {
+                int insult = (int) (Math.random() * 5) + 1;
+                if (insult == 1) t = t.replace("#가앙조", "겁나게");
+                else if (insult == 2) t = t.replace("#가앙조", "개");
+                else if (insult == 3) t = t.replace("#가앙조", "존나");
+                else if (insult == 4) t = t.replace("#가앙조", "존나게");
+                else if (insult == 5) t = t.replace("#가앙조", "쓸데없이");
+                else t = t.replace("#가앙조", "");
+            }
+            if (t.contains("#이상하기만")) {
+                int insult = (int) (Math.random() * 5) + 1;
+                if (insult == 1) t = t.replace("#이상하기만", "친구없어보이기만");
+                else if (insult == 2) t = t.replace("#이상하기만", "찐따같기만");
+                else if (insult == 3) t = t.replace("#이상하기만", "병신같기만");
+                else if (insult == 4) t = t.replace("#이상하기만", "이상하기만");
+                else if (insult == 5) t = t.replace("#이상하기만", "뇌에 쥐가 난 거 같기만");
+                else t = t.replace("#이상하기만", "좀 아픈 사람인 거 같기만");
+            }
+            if (t.contains("#이상하게")) {
+                int insult = (int) (Math.random() * 5) + 1;
+                if (insult == 1) t = t.replace("#이상하게", "친구 없는 것처럼");
+                else if (insult == 2) t = t.replace("#이상하게", "찐따같이");
+                else if (insult == 3) t = t.replace("#이상하게", "병신같이");
+                else if (insult == 4) t = t.replace("#이상하게", "이상하게");
+                else if (insult == 5) t = t.replace("#이상하게", "좆같이");
+                else t = t.replace("#이상하게", "좀 아픈 사람인 것 같이");
+            }
+            if (t.contains("#뭐만하면")) {
+                int what = (int) (Math.random() * 4) + 1;
+                if (what == 1) t = t.replace("#뭐만하면", "뭐만 하면");
+                else if (what == 2) t = t.replace("#뭐만하면", "맨날");
+                else if (what == 3) t = t.replace("#뭐만하면", "하루에 최소 한 번씩은");
+                else if (what == 4) t = t.replace("#뭐만하면", "1%라도 지 맘에 안 들면 무조건");
+                else if (what == 5) t = t.replace("#뭐만하면", "매일매일");
+            }
+
+
+            sb.append(t).append(" 민수정\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ fuck/src/main/java/run.java
@@ -0,0 +1,5 @@
+public class run extends Main {
+    public static void main(String[] args) {
+        System.out.println(Random_Fuck_Min_Soo_Jung(111));
+    }
+}
